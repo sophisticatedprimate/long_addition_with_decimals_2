f3bcb5210ac57beced2a415f2d3292cd97abac89
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-19 16:43:59 -0500

    Add files via upload

---
+++ long_addition_with_decimals_2.py
@@ -0,0 +1,139 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Refer to long_addition_with_decimals_1.py for further information on adding with decimals in python:
+https://github.com/sophisticatedprimate/long_addition_with_decimals_1/blob/main/long_addition_with_decimals_1.py
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed
+    2. Navigate to a directory containing this file (long_addition_with_decimals_2.py)
+    3. Run the program with the following command: python3 long_addition_with_decimals_2.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+# Problem 1  
+sum = 812.114 + 61.82
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(812.114,61.82,sum))
+
+# Problem 2  
+sum = 910.811 + 6.49
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(910.811,6.49,sum))
+
+# Problem 3  
+sum = 210.89 + 62.318
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(210.89,62.318,sum))
+
+# Problem 4  
+sum = 95.516 + 612.611
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(95.516,612.611,sum))
+
+# Problem 5  
+sum = 9.52 + 0.578
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(9.52,0.578,sum))
+
+# Problem 6  
+sum = 15.718 + 21.94
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(15.718,21.94,sum))
+
+# Problem 7  
+sum = 68.112 + 31.49
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(68.112,31.49,sum))
+
+# Problem 8  
+sum = 311.216 + 21.86
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(311.216,21.86,sum))
+
+# Problem 9  
+sum = 5.01 + 412.616
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(5.01,412.616,sum))
+
+# Problem 10 
+sum = 9.71 + 439.9
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(9.71,439.9,sum))
+
+# Problem 11 
+sum = 91.78 + 98.78
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(91.78,98.78,sum))
+
+# Problem 12 
+sum = 333.7 + 0.4458
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(333.7,0.4458,sum))
+
+# Problem 13 
+sum = 58.56 + 5.112
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(58.56,5.112,sum))
+
+# Problem 14 
+sum = 210.516 + 79.812
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(210.516,79.812,sum))
+
+# Problem 15 
+sum = 17.417 + 24.919
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(17.417,24.919,sum))
+
+# Problem 16 
+sum = 94.16 + 410.12
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(94.16,410.12,sum))
+
+# Problem 17 
+sum = 28.211 + 64.716
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(28.211,64.716,sum))
+
+# Problem 18 
+sum = 96.313 + 21.59
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(96.313,21.59,sum))
+
+# Problem 19 
+sum = 812.46 + 38.95
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(812.46,38.95,sum))
+
+# Problem 20 
+sum = 410.21 + 17.78
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(410.21,17.78,sum))
+
+# Problem 21 
+sum = 190.5 + 9.038
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(190.5,9.038,sum))
+
+# Problem 22 
+sum = 63.85 + 10.88
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(63.85,10.88,sum))
+
+# Problem 23 
+sum = 5.67 + 18.42
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(5.67,18.42,sum))
+
+# Problem 24 
+sum = 15.318 + 33.719
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(15.318,33.719,sum))
+
+# Problem 25 
+sum = 69.12 + 211.215
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(69.12,211.215,sum))
+
+# Problem 26 
+sum = 18.613 + 7.28
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(18.613,7.28,sum))
+
+# Problem 27 
+sum = 0.514 + 53.02
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(0.514,53.02,sum))
+
+# Problem 28 
+sum = 210.81 + 65.818
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(210.81,65.818,sum))
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_addition_with_decimals_2/issues
+"""
